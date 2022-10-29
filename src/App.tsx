65069998c9056f834ef39b2d241d2c44f2686ddc
telonasport99 <telonasport99@gmail.com>
2022-10-29 15:48:47 +0300

    3

--- src/App.tsx
+++ src/App.tsx
@@ -2,38 +2,40 @@ import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Button} from "./components/Button";
+
 type ShowType = {
-  userId:number
-  id:number
-  title:string
-  completed:boolean
+    userId: number
+    id: number
+    title: string
+    completed: boolean
 }
+
 function App() {
-  const [show,setShow]=useState<ShowType[]>([])
-  const showOff=()=>{
-      setShow([])
-  }
-  const showUp=()=>{
-    fetch('https://jsonplaceholder.typicode.com/todos')
-        .then(response => response.json())
-        .then(json => setShow(json))
-  }
-  return (
-    <div className="App">
-        <Button name={'ShowOff'} callback={showOff}/>
-      <ul>
-        {show.map(el=>{
-          return(
-              <li key={el.id}>
-                <span>{el.title}</span>
-                <span>{el.completed}</span>
-              </li>
-          )
-        })}
-      </ul>
-    <Button name={'Show'} callback={showUp}/>
-    </div>
-  );
+    const [show, setShow] = useState<ShowType[]>([])
+    const showOff = () => {
+        setShow([])
+    }
+    const showUp = () => {
+        fetch('https://jsonplaceholder.typicode.com/todos')
+            .then(response => response.json())
+            .then(json => setShow(json))
+    }
+    return (
+        <div className="App">
+            <Button name={'ShowOff'} callback={showOff}/>
+            <ul>
+                {show.map(el => {
+                    return (
+                        <li key={el.id}>
+                            <span>{el.title}</span>
+                            <span>{el.completed}</span>
+                        </li>
+                    )
+                })}
+            </ul>
+            <Button name={'Show'} callback={showUp}/>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-type ButtonPropsType={
-    name:string
-    callback:()=>void
+
+type ButtonPropsType = {
+    name: string
+    callback: () => void
 }
-export const Button = (props:ButtonPropsType) => {
-    const {name,callback}=props
-  const  onClickHandler =()=>{
-      callback()
-  }
+export const Button = (props: ButtonPropsType) => {
+    const {name, callback} = props
+    const onClickHandler = () => {
+        callback()
+    }
 
     return (
-     <button onClick={onClickHandler}>{name}</button>
+        <button onClick={onClickHandler}>{name}</button>
     );
 };
 
