50ca6c1512fd95c87625a8a69b20cfc29273263f
telonasport99 <telonasport99@gmail.com>
2022-10-29 15:33:07 +0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import {Button} from "./components/Button";
+type ShowType = {
+  userId:number
+  id:number
+  title:string
+  completed:boolean
+}
 function App() {
+  const [show,setShow]=useState<ShowType[]>([])
+  const showOff=()=>{
+      setShow([])
+  }
+  const showUp=()=>{
+    fetch('https://jsonplaceholder.typicode.com/todos')
+        .then(response => response.json())
+        .then(json => setShow(json))
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Button name={'ShowOff'} callback={showOff}/>
+      <ul>
+        {show.map(el=>{
+          return(
+              <li key={el.id}>
+                <span>{el.title}</span>
+                <span>{el.completed}</span>
+              </li>
+          )
+        })}
+      </ul>
+    <Button name={'Show'} callback={showUp}/>
     </div>
   );
 }
